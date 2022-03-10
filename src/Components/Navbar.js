5126c372bcdc78c61a61e2de34ecd47e4ee1b888
Meenakshi0710 <meenakshi.motiyani@gmail.com>
2022-03-10 09:46:25 +0530

    restore

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
 
-      <nav>
+      <nav className = "fixed-top">
       <input type="checkbox" id="check"/>
       <label htmlFor="check" className="checkbtn">
         <i className="fas fa-bars"></i>
@@ -12,12 +13,12 @@ export default function Navbar() {
       <img src = "Data/images/logo.jpg" width = "150px" height = "150px"/>
       </label>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/">About</a></li>
-        <li><a href="/">Services</a></li>
-        <li><a href="/">Products</a></li>
-        <li><a href="/">Harvest Chart</a></li>
-        <li><a href="/">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/">Services</Link></li>
+        <li><Link to="/">Products</Link></li>
+        <li><Link to="/">Harvest Chart</Link></li>
+        <li><Link to="/">Contact</Link></li>
       </ul>
     </nav>
       
